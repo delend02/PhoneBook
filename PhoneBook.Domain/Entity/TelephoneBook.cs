008f8f389703e5168d6a3da71963d602bfb9d992
tokarevmaksim2002@gmail.com <tokarevmaksim2002@gmail.com>
2022-08-07 16:10:25 +0200

    add entities

---
+++ PhoneBook.Domain/Entity/TelephoneBook.cs
@@ -0,0 +1,13 @@
+﻿namespace PhoneBook.Domain.Entity
+{
+    public class TelephoneBook
+    {
+        public ulong ID { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string SurName { get; set; }
+        public string NumberPhone { get; set; }
+        public string Address { get; set; }
+        public TelephoneDescription Description { get; set; }
+    }
+}
---
+++ PhoneBook.Domain/Entity/TelephoneDescription.cs
@@ -0,0 +1,10 @@
+﻿namespace PhoneBook.Domain.Entity
+{
+    public class TelephoneDescription
+    {
+        public ulong ID { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string SurName { get; set; }
+    }
+}
