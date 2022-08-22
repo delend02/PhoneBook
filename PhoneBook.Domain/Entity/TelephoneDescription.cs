3c02c192fd52116f5ed6a4cd2c3054ac9390a6a5
tokarevmaksim2002@gmail.com <tokarevmaksim2002@gmail.com>
2022-08-22 22:32:19 +0200

    new css, new view

--- PhoneBook.Domain/Entity/TelephoneDescription.cs
+++ PhoneBook.Domain/Entity/TelephoneDescription.cs
@@ -2,6 +2,14 @@
 {
     public class TelephoneDescription
     {
+        public TelephoneDescription(ulong iD, string lastName, string firstName, string surName)
+        {
+            ID = iD;
+            LastName = lastName;
+            FirstName = firstName;
+            SurName = surName;
+        }
+
         public ulong ID { get; set; }
         public string LastName { get; set; }
         public string FirstName { get; set; }
