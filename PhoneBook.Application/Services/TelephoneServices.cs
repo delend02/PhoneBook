12f8fa94a8ba69a56b7162d8dc503ec75ea1a4fe
tokarevmaksim2002@gmail.com <tokarevmaksim2002@gmail.com>
2022-09-05 15:06:13 +0200

    redaction window

---
+++ PhoneBook.Application/Services/TelephoneServices.cs
@@ -0,0 +1,12 @@
+﻿using PhoneBook.Domain.Entity;
+
+namespace PhoneBook.Application.Services
+{
+    public class TelephoneServices : ITelephoneServices
+    {
+        public Task<IEnumerable<TelephoneBook>> SearchTelephone(string phoneNumber)
+        {
+            return null;
+        }
+    }
+}
