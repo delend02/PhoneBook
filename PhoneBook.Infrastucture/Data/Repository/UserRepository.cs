445127dd6f15094ed2cc3089e294151a5f68af1f
tokarevmaksim2002@gmail.com <tokarevmaksim2002@gmail.com>
2022-09-06 18:16:19 +0200

    add new entity

---
+++ PhoneBook.Infrastucture/Data/Repository/UserRepository.cs
@@ -0,0 +1,55 @@
+﻿using PhoneBook.Domain.Entity;
+using PhoneBook.Domain.Interfaces;
+using PhoneBook.Infrastucture.Data.Context;
+
+namespace PhoneBook.Infrastucture.Data.Repository
+{
+    public class UserRepository : IRepository<User>
+    {
+        private PhoneContext _db;
+
+        public UserRepository(PhoneContext db)
+        {
+            _db = db;
+        }
+
+        public void Create(User item)
+        {
+            _db.Users.Add(item);
+        }
+
+        public void Delete(ulong id)
+        {
+            var user = _db.Users.Find(id);
+            if (user is not null)
+                _db.Users.Remove(user);
+        }
+
+        public User Find(Func<User, bool> predicate)
+        {
+            return _db.Users.Find(predicate);
+        }
+
+        public IEnumerable<User> GetAll()
+        {
+            var result = _db.Users.ToList();
+            return result;
+        }
+
+        public User GetByID(ulong id)
+        {
+            var result = _db.Users.Find(id);
+            return result;
+        }
+
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+
+        public void Update(User item)
+        {
+            _db.Users.Update(item);
+        }
+    }
+}
