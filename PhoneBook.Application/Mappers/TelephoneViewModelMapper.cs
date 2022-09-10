d105c096e5a910b238863612f8e986806c207243
tokarevmaksim2002@gmail.com <tokarevmaksim2002@gmail.com>
2022-09-10 16:02:42 +0200

    create mappers and new viewmodel

---
+++ PhoneBook.Application/Mappers/TelephoneViewModelMapper.cs
@@ -0,0 +1,31 @@
+﻿using PhoneBook.Application.ViewModel;
+using PhoneBook.Domain.Entity;
+
+namespace PhoneBook.Application.Mappers
+{
+    public class TelephoneViewModelMapper
+    {
+        public IEnumerable<TelephoneViewModel> ConstructFromListOfEntities(IEnumerable<TelephoneBook> books)
+        {
+            var telephoneViewModel = books.Select(item => new TelephoneViewModel
+            {
+                FirstName = item.FirstName,
+                LastName = item.LastName,
+                NumberPhone = item.NumberPhone
+            }
+            );
+
+            return telephoneViewModel;
+        }
+
+        public TelephoneViewModel ConstructFromEntity(TelephoneBook book)
+        {
+            return new TelephoneViewModel
+            {
+                FirstName = book.FirstName,
+                NumberPhone = book.NumberPhone,
+                LastName = book.LastName
+            };
+        }
+    }
+}
