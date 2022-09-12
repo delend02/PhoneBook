3a91159562f66591fb4da78ffa07704dfecaf443
tokarevmaksim2002@gmail.com <tokarevmaksim2002@gmail.com>
2022-09-12 15:31:02 +0200

    delete mapper

--- PhoneBook.Application/Services/ITelephoneServices.cs
+++ PhoneBook.Application/Services/ITelephoneServices.cs
@@ -1,12 +1,7 @@
-﻿using PhoneBook.Application.ViewModel;
-
-namespace PhoneBook.Application.Services
+﻿namespace PhoneBook.Application.Services
 {
     public interface ITelephoneServices
     {
-        Task<IEnumerable<DescriptionViewModel>> GetAll();
-        Task<TelephoneViewModel> GetById(ulong id);
-        Task<TelephoneViewModel> Create(TelephoneViewModel taskViewModel);
-        Task Delete(ulong id);
+        
     }
 }
--- PhoneBook.Application/Services/TelephoneServices.cs
+++ PhoneBook.Application/Services/TelephoneServices.cs
@@ -1,39 +1,7 @@
-﻿using PhoneBook.Application.Mappers;
-using PhoneBook.Application.ViewModel;
-using PhoneBook.Domain.Entity;
-using PhoneBook.Domain.Interfaces;
-
-namespace PhoneBook.Application.Services
+﻿namespace PhoneBook.Application.Services
 {
     public class TelephoneServices : ITelephoneServices
     {
-        private readonly TelephoneViewModelMapper _telephoneViewModelMapper;
-        private readonly IRepository<TelephoneBook> _telephoneRepository;
-
-        public TelephoneServices(TelephoneViewModelMapper telephoneViewModelMapper, IRepository<TelephoneBook> telephoneRepository)
-        {
-            _telephoneViewModelMapper = telephoneViewModelMapper;
-            _telephoneRepository = telephoneRepository;
-        }
-
-        public Task<TelephoneViewModel> Create(TelephoneViewModel taskViewModel)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task Delete(ulong id)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<IEnumerable<DescriptionViewModel>> GetAll()
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<TelephoneViewModel> GetById(ulong id)
-        {
-            throw new NotImplementedException();
-        }
+        
     }
 }
