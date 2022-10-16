72526bc2f539822d67d8ce6819b1ec34e96ef981
tokarevmaksim2002@gmail.com <tokarevmaksim2002@gmail.com>
2022-10-17 00:13:50 +0200

    Develop main window

--- PhoneBook.WPF/ViewModels/MainWindowViewModel.cs
+++ PhoneBook.WPF/ViewModels/MainWindowViewModel.cs
@@ -1,7 +1,53 @@
-﻿namespace PhoneBook.WPF.ViewModels
+﻿using PhoneBook.WPF.ViewModels.Command;
+using System.Windows.Input;
+
+namespace PhoneBook.WPF.ViewModels
 {
     internal class MainWindowViewModel : ViewModel
     {
+        public MainWindowViewModel()
+        {
+            Add = new LamdaCommand(OnAdd, CanAdd);
+            Search = new LamdaCommand(OnSearch, CanSearch);
+            Table = new LamdaCommand(OnTable, CanTable);
+        }
 
+        #region Button
+        public ICommand Add { get; }
+
+        private void OnAdd(object p)
+        {
+
+        }
+
+        private bool CanAdd(object p) => true;
+        
+        public ICommand Search { get; }
+
+        private void OnSearch(object p)
+        {
+
+        }
+
+        private bool CanSearch(object p) => true;
+
+        public ICommand Table { get; }
+
+        private void OnTable(object p)
+        {
+
+        }
+
+        private bool CanTable(object p) => true;
+        #endregion
+
+
+        private string _textHeader;
+
+        public string TextHeader
+        { 
+            get => _textHeader;
+            set => Set(ref _textHeader, value);
+        }
     }
 }
