be8bc67cc66c8511cd0d80802f880c918820eb30
tokarevmaksim2002@gmail.com <tokarevmaksim2002@gmail.com>
2022-10-30 14:08:43 +0200

    1

---
+++ PhoneBook.ApiInterLayer/Endpoints.cs
@@ -0,0 +1,9 @@
+﻿namespace PhoneBook.ApiInterLayer
+{
+    public class Endpoints
+    {
+        public const string Api = "api";
+        public const string User = $"{Api}/user";
+        public const string Phone = $"{Api}/Phone";
+    }
+}
