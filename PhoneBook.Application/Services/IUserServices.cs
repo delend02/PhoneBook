94598d2e3a71bc090b4bf376f387ff276778d992
tokarevmaksim2002@gmail.com <tokarevmaksim2002@gmail.com>
2022-11-05 22:28:55 +0200

    1

--- PhoneBook.Application/Services/IUserServices.cs
+++ PhoneBook.Application/Services/IUserServices.cs
@@ -1,7 +1,12 @@
-﻿namespace PhoneBook.Application.Services
+﻿using PhoneBook.Domain.Entity;
+
+namespace PhoneBook.Application.Services
 {
     public interface IUserServices
     {
-
+        User Get(ulong ID);
+        void Update(User user);
+        void Delete(ulong id);
+        void Create(User user);
     }
 }
