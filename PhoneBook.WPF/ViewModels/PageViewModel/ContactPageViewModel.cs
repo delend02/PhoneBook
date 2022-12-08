8f4807ab4f818e006550d5ce179a9a9848712605
tokarevmaksim2002@gmail.com <tokarevmaksim2002@gmail.com>
2022-12-08 21:36:31 +0200

    1

--- PhoneBook.WPF/ViewModels/PageViewModel/ContactPageViewModel.cs
+++ PhoneBook.WPF/ViewModels/PageViewModel/ContactPageViewModel.cs
@@ -1,7 +1,16 @@
-﻿namespace PhoneBook.WPF.ViewModels.PageViewModel
+﻿using PhoneBook.ApiInterLayer.Models;
+using PhoneBook.Domain.Entity;
+using PhoneBook.WPF.Service;
+using PhoneBook.WPF.ViewModels.Command;
+using System.Windows.Input;
+
+namespace PhoneBook.WPF.ViewModels.PageViewModel
 {
     internal class ContactPageViewModel : PageViewModelBase
     {
+        public ContactPageViewModel()
+        {
 
+        }
     }
 }
