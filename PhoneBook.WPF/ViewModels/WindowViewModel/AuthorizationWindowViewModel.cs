4ee345c66f293457172052759e9e2e14d1040ae8
tokarevmaksim2002@gmail.com <tokarevmaksim2002@gmail.com>
2023-01-18 21:52:35 +0200

    1

---
+++ PhoneBook.WPF/ViewModels/WindowViewModel/AuthorizationWindowViewModel.cs
@@ -0,0 +1,56 @@
+﻿using PhoneBook.WPF.ViewModels.Command;
+using System.Windows.Input;
+
+namespace PhoneBook.WPF.ViewModels.WindowViewModel
+{
+    internal class AuthorizationWindowViewModel : WindowViewModelBase
+    {
+        public AuthorizationWindowViewModel()
+        {
+            Enter = new LamdaCommand(OnEnter, CanEnter);
+            Registration = new LamdaCommand(OnRegistration, CanRegistration);
+            Cancel = new LamdaCommand(OnCancel, CanCancel);
+        }
+
+        public ICommand Enter { get; }
+
+        private async void OnEnter(object p)
+        {
+            
+        }
+
+        private bool CanEnter(object p) => true;
+
+        public ICommand Registration { get; }
+
+        private async void OnRegistration(object p)
+        {
+            
+        }
+
+        private bool CanRegistration(object p) => true;
+        
+        public ICommand Cancel { get; }
+
+        private async void OnCancel(object p)
+        {
+            
+        }
+
+        private bool CanCancel(object p) => true;
+
+        private string _login;
+        public string Login
+        {
+            get => _login;
+            set => Set(ref _login, value);
+        }
+        
+        private string _password;
+        public string Password
+        { 
+            get => _password; 
+            set => Set(ref _password, value); 
+        }
+    }
+}
