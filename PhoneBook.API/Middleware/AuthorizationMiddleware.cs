2cb30ee5eb0c9c0cb2fb936d7d5df698ade6d366
tokarevmaksim2002@gmail.com <tokarevmaksim2002@gmail.com>
2023-02-05 21:57:52 +0200

    1

--- PhoneBook.API/Middleware/AuthorizationMiddleware.cs
+++ PhoneBook.API/Middleware/AuthorizationMiddleware.cs
@@ -19,19 +19,19 @@ namespace PhoneBook.API.Middleware
 
         public async Task InvokeAsync(HttpContext context)
         {
-            var privateKey = _configuration.GetSection("PrivateKeys").GetValue<string>("JwtToken");
-            await _next(context);
+            //var privateKey = _configuration.GetSection("PrivateKeys").GetValue<string>("JwtToken");
+            //await _next(context);
 
-            var manager = new Manager(privateKey);
+            //var manager = new Manager(privateKey);
 
-            var isAuth = manager.ValidateToken(context.Request.Cookies["token"], out JwtSecurityToken jwtToken);
+            //var isAuth = manager.ValidateToken(context.Request.Cookies["token"], out JwtSecurityToken jwtToken);
 
-            if (isAuth is not false)
-            {
-                context.Response.StatusCode = StatusCodes.Status401Unauthorized;
-                await context.Response.WriteAsJsonAsync(new { Error = "Необходима авторизация" });
-                return;
-            }
+            //if (isAuth is not false)
+            //{
+            //    context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+            //    await context.Response.WriteAsJsonAsync(new { Error = "Необходима авторизация" });
+            //    return;
+            //}
 
             
         }
